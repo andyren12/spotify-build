d25dce96aca503e918871b7177dc288848610589
Andy Ren <andyren33@gmail.com>
2023-05-25 15:06:47 -0700

    Create log out dropdown

--- components/Center.js
+++ components/Center.js
@@ -6,19 +6,28 @@ import Playlist from "./Playlist";
 import Home from "./Home";
 import { useRecoilValue } from "recoil";
 import { displayState } from "@/atoms/displayAtom";
+import { useState } from "react";
 
 export default function Center() {
   const { data: session } = useSession();
   const display = useRecoilValue(displayState);
+  const [dropDownStyle, setDropDownStyle] = useState("opacity-0");
+
+  const handleDropDown = () => {
+    if (dropDownStyle === "opacity-0") {
+      setDropDownStyle("opacity-1");
+    } else {
+      setDropDownStyle("opacity-0");
+    }
+  };
 
   return (
     <div className="flex-grow h-screen overflow-y-scroll scrollbar-hide">
-      <header className="absolute top-5 right-8">
+      <header className="absolute top-5 right-8 cursor-pointer">
         <div
           className="flex items-center 
-        bg-black space-x-3 opacity-90 hover:opacity-80 
-        cursor-pointer rounded-full p-1 pr-2 text-white"
-          onClick={signOut}
+        bg-gray-900 space-x-3 opacity-90 hover:opacity-80 rounded-full p-1 pr-2 text-white"
+          onClick={handleDropDown}
         >
           <img
             className="rounded-full w-10 h-10"
@@ -27,6 +36,13 @@ export default function Center() {
           />
           <h2>{session?.user.name}</h2>
           <ChevronDownIcon className="h-5 w-5" />
+        </div>
+        <div
+          className={`${dropDownStyle} mt-2 p-1 bg-gray-900 text-center 
+          opacity-90 hover:opacity-80 transition-opacity rounded-full`}
+          onClick={signOut}
+        >
+          Logout
         </div>
       </header>
       {display === "playlist" && <Playlist />}
